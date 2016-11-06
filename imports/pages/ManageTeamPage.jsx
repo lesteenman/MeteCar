65b272fd1b39efe8c0f30353a618234f73d90aa4
Erik Steenman <eriksteenman@gmail.com>
2016-11-06 08:47:21 +0100

    Forgot files, add source for the Reactive Google Map

---
+++ imports/pages/ManageTeamPage.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { browserHistory } from 'react-router';
+
+import { Team } from '../api/Teams.jsx';
+
+import { InputLine, ActionButton, ExtraButton } from '../ui/UiComponents.jsx';
+import '../less/form.scss';
+
+class CreateTeamPage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {error: {}};
+		this.submit = this._submit.bind(this);
+	}
+
+	_submit(id) {
+		console.log('Update Team', id, this.refs.name.value(), this.refs.description.value());
+	}
+
+	render() {
+		if (this.props.loading) return (<div></div>);
+		let { nameError, descriptionError, error} = this.state.error;
+
+		return (
+			<div className='form-container'>
+				<Helmet title="Manage team" />
+
+				<InputLine ref='name' label='Team name' value={this.props.team.name} error={nameError} />
+				<InputLine ref='description' label='Team description' value={this.props.team.description} multiLine={true} error={descriptionError} />
+				<div style={{color: 'red'}}>
+					{error}
+				</div>
+
+				<ActionButton handler={this.submit.bind(this, this.props.team._id)}>Save</ActionButton>
+				<ExtraButton handler={browserHistory.goBack}>Cancel</ExtraButton>
+			</div>
+		);
+	}
+}
+
+export default createContainer(() => {
+	let teamHandle = Meteor.subscribe('Meteor.teams');
+	let team = Team.find({_id: Meteor.user().team});
+	return {
+		loading: !teamHandle.ready(),
+		team: team
+	};
+}, CreateTeamPage);
---
+++ imports/ui/google-map/lib/GoogleApi.js
@@ -0,0 +1,43 @@
+import invariant from 'invariant';
+
+export const GoogleApi = function(opts) {
+    opts = opts || {};
+
+    invariant(opts.hasOwnProperty('apiKey'),
+              'You must pass an apiKey to use GoogleApi');
+
+    const apiKey = opts.apiKey;
+    const libraries = opts.libraries || ['places'];
+    const client = opts.client;
+    const URL = 'https://maps.googleapis.com/maps/api/js';
+
+    const googleVersion = opts.version || '3.24';
+
+    let channel = null;
+    let language = null;
+    let region = null;
+
+    const url = () => {
+        let url = URL;
+        let params = {
+            key: apiKey,
+            callback: 'CALLBACK_NAME',
+            libraries: libraries.join(','),
+            client: client,
+            v: googleVersion,
+            channel: channel,
+            language: language,
+            region: region
+        };
+
+        let paramStr = Object.keys(params)
+            .filter(k => !!params[k])
+            .map(k => `${k}=${params[k]}`).join('&');
+
+        return `${url}?${paramStr}`;
+    };
+
+    return url();
+};
+
+export default GoogleApi;
